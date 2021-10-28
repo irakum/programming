bbab3c6174fb2c44c07ed256ba629b314244ad06
irakum <iringreen1@gmail.com>
2021-10-28 10:14:04 +0300

    lab4 done

---
+++ lab4/lab4/lab4.cpp
@@ -0,0 +1,29 @@
+﻿// lab4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <math.h>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float i, n;
+    cout << "Enter number of repetition: ";
+    cin >> n;
+
+    float x, y, sum;
+    x = 1;
+    y = 1;
+    sum = x / (1 + abs(y));
+
+    for (i = 1; i <= n; i++)
+    {
+        y += x;
+        x *= 0.3;
+        sum += x / (1 + abs(y));
+    }
+
+    cout << "Final summary: " << sum;
+}
+
