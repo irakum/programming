2858fe53ea9f2e3e5733ae555adbf49da917df6f
irakum <iringreen1@gmail.com>
2021-10-28 09:55:41 +0300

    lab3 done

---
+++ lab3/lab3/lab3.cpp
@@ -0,0 +1,28 @@
+﻿// lab3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <math.h>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float precision;
+    cout << "Enter precision: "; // введення точності
+    cin >> precision;
+
+    float y0, y;
+    y0 = 0;                      // початкове значення у
+    y = (y0 + 1) / (y0 + 2);
+
+    // розрахування значення у з заданою точністю
+    do
+    {
+        y0 = y;
+        y = (y0 + 1) / (y0 + 2);
+    } while (abs(y-y0) >= precision);
+
+    cout << "Final value: " << y;  // виведення кінцевого значення у
+}
+
