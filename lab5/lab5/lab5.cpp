49beb0f0f300e76bf30b26957f8565840115c271
irakum <iringreen1@gmail.com>
2021-11-18 10:46:34 +0200

    add comments

--- lab5/lab5/lab5.cpp
+++ lab5/lab5/lab5.cpp
@@ -4,15 +4,14 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     int max_fib = 100;
     
     int new_last, num_new, sec_last, last;
-    new_last = 1;
-    num_new = 1;
-    sec_last = 1;
-    last = 1;
+    new_last = 1;       //нове підраховане число Фібоначі
+    num_new = 1;        //номер цього числа 
+    sec_last = 1;       //передостаннє число в ряду Фібоначі
+    last = 1;           //останнє число в ряду Фібоначі
 
     while ((last + sec_last) <= max_fib)
     {
@@ -22,7 +21,8 @@ int main()
         last = new_last;
 
         int sum_i = 0;
-        
+
+        //перевірка на простоту
         for (int i = 1; i <= sqrt(new_last); i++)
         {
             if (new_last % i == 0) sum_i += 1;
