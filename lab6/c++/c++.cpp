1aaf5d083ea82556d129cb2965f8b153bbb86ecc
irakum <iringreen1@gmail.com>
2021-12-02 10:10:32 +0200

    lab 6 c++

---
+++ lab6/c++/c++.cpp
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+bool proste(int);
+void twins(int);
+
+int main()
+{
+    int n;
+    cout << "Enter your number: ";
+    cin >> n;
+    while (n < 1){
+        cout << "Wrong value. Enter a natural number: ";
+        cin >> n;
+    }
+
+    for (int i = n; i < 2*n-1; i++)
+    {
+        twins(i);
+    }
+}
+
+// перевірка, чи є число простим
+bool proste(int number) {
+    if (number <= 1) return false;
+    
+    int count = 0;
+    for (int i = 1; i <= sqrt(number); i++) {
+        if (number % i == 0) count++;
+    }
+    
+    if (count == 1) return true;
+    else return false;
+}
+
+// перевірка, чи є числа близнюками
+void twins(int number) {
+    if (proste(number) and proste(number + 2))
+    {
+        cout << number << " and " << number + 2 << endl;
+    }
+}
