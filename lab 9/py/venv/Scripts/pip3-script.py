17a7a78079b4023ebae28fa72ff6fba00ec9bc30
irakum <iringreen1@gmail.com>
2021-12-26 18:36:52 +0200

    added output of words

---
+++ lab 9/py/venv/Scripts/pip3-script.py
@@ -0,0 +1,12 @@
+#!"D:\1 курс\ОП\labs\laboratorna 1\lab 9\py\venv\Scripts\python.exe" -x
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip3'
+__requires__ = 'pip==19.0.3'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pip==19.0.3', 'console_scripts', 'pip3')()
+    )
