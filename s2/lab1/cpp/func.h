ed820094ad0a98bb303ca0635d64c497d2b10a9c
irakum <iringreen1@gmail.com>
2022-02-23 23:07:08 +0200

    lab 1 finished in c++

--- s2/lab1/cpp/func.h
+++ s2/lab1/cpp/func.h
@@ -7,24 +7,21 @@ using namespace std;
 /** Text from keyboard, until Alt+1 pressed
  * @return input text
  */
-string get_text();
+vector<string> get_text();
 /** Creates new file with selected text
  * @param name name of file
  * @param text to put in the file
  */
-void get_file(const string& name, const string& text);
+void get_file(const string& name, const vector<string>& text);
 /** Gets text from file
  * @param name name of file
  * @return text from the selected file
  */
-string read_file(const string& name);
-/** Splits text into lines
- * @param text selected text
- * @return vector with lines
- */
-vector<string> split_lines(const string& text);
+vector<string> read_file(const string& name);
 /** Puts len of the string in the beginning of each one
  * @param text started text
  * @return changed text
  */
-string change_text(const string& text);
+vector<string> change_text(const vector<string>& text);
+
+void output(const vector<string>& text);
